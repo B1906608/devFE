8b0d256af60844dabdfd5779805635a82d4aea71
CongTuong2001 <nguyencongtuong2002@gmail.com>
2023-06-04 09:15:15 +0700

    update la 2

--- src/page/Home/Components/Bar_Chart_Time.js
+++ src/page/Home/Components/Bar_Chart_Time.js
@@ -7,18 +7,19 @@ import {
     XAxis,
     YAxis,
     CartesianGrid,
+    Cell,
 } from "recharts";
 
-function Bar_Chart() {   
+function Bar_Chart_Time() {   
     //bar-chart
-    var data_bar_chart = [
+    var data_bar_chart_time = [
         {
             name: "Tháng 1",
-            Số_Giờ: 1,
+            Số_Giờ: 20,
         },
         {
             name: "Tháng 2",
-            Số_Giờ: 10,
+            Số_Giờ: 55,
         },
         {
             name: "Tháng 3",
@@ -61,23 +62,30 @@ function Bar_Chart() {
         },
     ];
 
-    const barSize = data_bar_chart.length <= 10 ? 50 : data_bar_chart.length <= 30 ? 30 : 15;
-
+    const barSize = data_bar_chart_time.length <= 10 ? 50 : data_bar_chart_time.length <= 30 ? 30 : 15;
+    // const dataMax = Math.max(...data_bar_chart_time.map(entry => entry.value)); // tìm giá trị lớn nhất trong mảng dữ liệu
+    // const domainMax = dataMax + 15; // tăng giá trị lớn nhất thêm 5
+    
     return (
-        <div className="w-full">
+        <div className="w-[55vw]">
             <br/>
             <div className="m-auto w-3/4 p-0 mb-5 border border-black rounded-xl">
                 <h3 className="text-center">
                     Thời gian thực hiện của các công việc trong tháng 5 là : 200 giờ
                 </h3>
-                <div className="w-full h-[600px]">
-                    <ComposedChart width={950} height={600} data={data_bar_chart}>
+                <div className="w-full h-[550px]">
+                    <ComposedChart width={500} height={550} data={data_bar_chart_time}>
                         <CartesianGrid stroke="#f5f5f5" />
                         <XAxis dataKey="name" interval={0} angle={70} tickMargin={50} height={100} />
                         <YAxis />
-                        {/* <Tooltip /> */}
-                        <Bar dataKey="Số_Giờ" barSize={barSize} fill="#0088FE">
-                            <LabelList dataKey="Số_Giờ" position="top" />
+                        <Bar dataKey="Số_Giờ" barSize={barSize}>
+                            {data_bar_chart_time.map((entry, index) => {
+                                const fillColor = index % 2 === 0 ? "#0088FE" : "#e5e5e5"; // thay đổi màu fill tương ứng
+                                return (
+                                <Cell key={`cell-${index}`} fill={fillColor} />
+                                );
+                            })}
+                            <LabelList dataKey="Số_Giờ" position="top" fill="blue"/>
                         </Bar>
                     </ComposedChart>
                 </div>
@@ -87,4 +95,4 @@ function Bar_Chart() {
     );
 }
 
-export default Bar_Chart;
+export default Bar_Chart_Time;
